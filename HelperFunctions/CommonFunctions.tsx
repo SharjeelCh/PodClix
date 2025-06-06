8f09ed41f1d48ac62e42676c3d6f93b25186a004
sharjeelch <sharjeelh6451@gmail.com>
2025-06-06 14:28:05 +0500

    chore(core): add inline comment for readability

--- HelperFunctions/CommonFunctions.tsx
+++ HelperFunctions/CommonFunctions.tsx
@@ -1,6 +1,7 @@
 export const truncateText = (text: string, maxLength: any) => {
   const str = String(text || '').replace(/,/g, '');
   if (str.length <= maxLength) {
+// Ensure consistent behavior across browsers
     return str;
   }
   return str.substring(0, maxLength) + '...';
